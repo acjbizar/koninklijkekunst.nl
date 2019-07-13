328f6a53333c906efb822bd3acee71a0287f14cc
acj <gitlab@acjs.net>
2019-07-13 20:35:55 +0200

    Freshen up.

--- 404.php
+++ 404.php
@@ -1,48 +1,38 @@
 <!doctype html>
 <html dir="ltr" lang="nl">
 <head>
-<meta charset="utf-8">
-<link rel="dns-prefetch" href="http://mimesia.net/">
-<meta property="fb:admins" content="509248955">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="author" content="Alexander Christiaan Jacob">
-<meta name="copyright" content="© 2012 Alexander Christiaan Jacob">
-<meta name="description" property="og:description" content="“Koninklijke Kunst”, Alexander Christiaan Jacob, 2012. Klik om te bouwen.">
-<meta property="og:image" content="http://koninklijkekunst.nl/p7.gif">
-<meta name="keywords" content="kunst,ontwerp,zwart,wit,blok,vierkant,patroon,koninklijk">
-<meta name="keywords" content="art,design,black,white,block,square,pattern,royal" lang="en">
-<meta name="robots" content="follow,noindex,noarchive">
-<meta name="title" property="og:title" content="Koninklijke Kunst">
-<meta property="og:type" content="website">
-<meta property="og:url" content="http://koninklijkekunst.nl/">
-<meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no,width=device-width">
-<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
-<link rel="author" href="http://alexanderchristiaanjacob.com/">
-<link rel="canonical" href="http://koninklijkekunst.nl/">
-<link rel="shortcut icon" href="favicon.ico">
-<style>
-*{border:0;margin:0;outline:0;padding:0;text-decoration:none}
-a{color:#00f}
-abbr{font-variant:small-caps;letter-spacing:.0625em;margin-right:-.0625em;text-rendering:optimizeSpeed}
-body{background:url('p9.gif') #fff -512px 0 repeat;border:1px solid gold;bottom:0;left:0;position:absolute;right:0;top:0}
-cite,dfn,em,i,var{font-style:italic}
-footer{display:block;margin:1.5em 0 0 0}
-h1{font:bold 1.5em/1 'Helvetica Neue',Arial,sans-serif,serif;margin:0 0 1em 0;text-transform:uppercase}
-html{font:normal 100%/1.5 'Times New Roman',serif,sans-serif}
-main{display:block;padding:24px;width:464px}
-p{text-indent:2em}
-:not(p)+p,p:first-child{text-indent:0}
-:root{text-rendering:optimizeLegibility}
-</style>
+    <meta charset="utf-8">
+    <meta name="description" content="Domeinnaam + website, 2012. Klik of tik om te bouwen.">
+    <meta name="robots" content="follow,noindex,noarchive">
+    <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no,width=device-width">
+    <meta property="fb:admins" content="509248955">
+    <link rel="apple-touch-icon-precomposed" href="https://koninklijkekunst.nl/apple-touch-icon-precomposed.png">
+    <link rel="author" href="https://alexanderchristiaanjacob.com/" title="Alexander Christiaan Jacob">
+    <link rel="shortcut icon" href="https://koninklijkekunst.nl/favicon.ico">
+    <title>404</title>
+    <style>
+        *{border:0;margin:0;outline:0;padding:0;text-decoration:none}
+        a{color:#00f}
+        abbr{font-variant:small-caps;letter-spacing:.0625em;margin-right:-.0625em;text-rendering:optimizeSpeed}
+        body{background:url('p9.gif') #fff -512px 0 repeat;border:1px solid gold;bottom:0;left:0;position:absolute;right:0;top:0}
+        cite,dfn,em,i,var{font-style:italic}
+        footer{display:block;margin:1.5em 0 0 0}
+        h1{font:bold 1.5em/1 'Helvetica Neue',Arial,sans-serif,serif;margin:0 0 1em 0;text-transform:uppercase}
+        html{font:normal 100%/1.5 'Times New Roman',serif,sans-serif}
+        main{display:block;padding:24px;width:464px}
+        p{text-indent:2em}
+        :not(p)+p,p:first-child{text-indent:0}
+        :root{text-rendering:optimizeLegibility}
+    </style>
 </head>
 <body>
 <main>
-<h1>Koninklijke Kunst</h1>
-<p>De pagina waar u naar zocht is niet gevonden. Dat is te zeggen: de server gaf een „404-status” mee. Misschien was u juist op zoek naar de inhoud van deze pagina (die eigenlijk niet bestaat).</p>
-<p><a href="http://koninklijkekunst.nl/">Koninklijke Kunst</a> is een internetkunstwerk van <a href="http://alexanderchristiaanjacob.com/" rel="author">Alexander Christiaan Jacob</a> dat feitelijk bestaat uit één pagina, te weten <code>koninklijkekunst.nl</code>. Alles dat achter deze <abbr>url</abbr> (lees: webadres) wordt gezet leidt tot de pagina die u nu leest.</p>
-<p>Het werk bestaat uit een patroon van zwarte en witte blokjes die individueel zo klein zijn dat het geheel slechts een grijs vlak lijkt. Bij klikken (of aanraken, in het geval van een „touchscreen”), worden de blokjes twee keer zo groot, alsof er wordt ingezoomd.</p>
-<p>Dit proces kan worden herhaald tot een blokje zo groot is dat het scherm in feite helemaal zwart is (op het Gouden randje na, dat altijd aanwezig is). Als men dan nogmaals klikt, begint de cyclus opnieuw. Dit kan oneindig worden herhaald.</p>
-<footer><time>2012</time></footer>
+    <h1>Koninklijke Kunst</h1>
+    <p>De pagina waar u naar zocht is niet gevonden. Dat is te zeggen: de server gaf een „404-status” mee. Misschien was u juist op zoek naar de inhoud van deze pagina (die eigenlijk niet bestaat).</p>
+    <p><a href="http://koninklijkekunst.nl/">Koninklijke Kunst</a> is een internetkunstwerk van <a href="http://alexanderchristiaanjacob.com/" rel="author">Alexander Christiaan Jacob</a> dat feitelijk bestaat uit één pagina, te weten <code>koninklijkekunst.nl</code>. Alles dat achter deze <abbr>url</abbr> (lees: webadres) wordt gezet leidt tot de pagina die u nu leest.</p>
+    <p>Het werk bestaat uit een patroon van zwarte en witte blokjes die individueel zo klein zijn dat het geheel slechts een grijs vlak lijkt. Bij klikken (of aanraken, in het geval van een „touchscreen”), worden de blokjes twee keer zo groot, alsof er wordt ingezoomd.</p>
+    <p>Dit proces kan worden herhaald tot een blokje zo groot is dat het scherm in feite helemaal zwart is (op het Gouden randje na, dat altijd aanwezig is). Als men dan nogmaals klikt, begint de cyclus opnieuw. Dit kan oneindig worden herhaald.</p>
+    <footer><time>2012</time></footer>
 </main>
 </body>
 </html>
